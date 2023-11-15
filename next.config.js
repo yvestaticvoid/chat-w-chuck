9340665ea1f412be8f97e734daa15844774f0fdf
yves <yvesmotta.contacy@gmail.com>
2023-11-15 16:49:32 -0300

    basic layout and functioning; review state

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig, module.exports = {
+  images: {
+    domains: ['wl-genial.cf.tsp.li'],
+  },
+};
